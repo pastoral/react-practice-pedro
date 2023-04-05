ac2ece4e969a99d51c60a9ba5f3e9b3b035c9e71
Munir <munir@Munirs-MacBook-Pro.local>
2023-04-06 00:07:58 +0600

    Basic add task function added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[todoList,setTodoList] = useState([])
+  const[newTask, setNewTask] = useState('')
+
+  const addTask = () =>{
+    let newTodoList = [...todoList, newTask]
+    setTodoList(newTodoList)
+  }
   return (
     <div className="App">
       <p>Hollla Amigo</p>
+      <div className="createTask">
+        <input onChange={
+          (event)=>{
+            setNewTask(event.target.value)
+          }
+        }/>
+        <button onClick={addTask}> add task </button>
+       
+      </div>
+      <div className="taskList">
+        {todoList.map((task)=>{
+          return (<h4>{task}</h4>)
+        }   
+        )}
+      </div>
     </div>
   );
 }
