a713429bd6c1f3c0b975defcd1b5d95b9fe7c7ec
Munir <munir@Munirs-MacBook-Pro.local>
2023-04-07 16:26:41 +0600

    Delete function modified
    Now todoList is an array of object rather than array of String

--- src/App.js
+++ src/App.js
@@ -8,10 +8,17 @@ function App() {
   const addTask = () =>{
     // let newTodoList = [...todoList, newTask]
     // setTodoList(newTodoList)
-    setTodoList([...todoList, newTask])
+
+    //setTodoList([...todoList, newTask])
+
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length-1].id +1 ,
+      taskName : newTask
+    }
+    setTodoList([...todoList,task])
   }
 
-  const deleteTask = (taskName) =>{
+  const deleteTask = (id) =>{   // (taskName) =>{
     // ##### Old Code ########
     // const newTodoList = todoList.filter((t)=>{
     //     if(t===taskName){
@@ -34,7 +41,10 @@ function App() {
    //######## modification 2 ########
   // const newTodoList = todoList.filter((t)=> t !== taskName)
   // setTodoList(newTodoList)
-  setTodoList(todoList.filter((t)=> t !== taskName))
+
+  // setTodoList(todoList.filter((t)=> t !== taskName))
+
+  setTodoList(todoList.filter((t)=> t.id !== id))
    
   }
 
@@ -54,8 +64,8 @@ function App() {
         {todoList.map((task)=>{
           return (
             <div> 
-              <h4>{task}</h4>
-              <a href = "#"><button onClick={()=>deleteTask(task)}>X</button></a>
+              <h4>{task.taskName}</h4>
+              <a href = "#"><button onClick={()=>deleteTask(task.id)}>X</button></a>
             </div>
             
             
